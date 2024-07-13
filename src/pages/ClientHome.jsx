9be6d8bab3a03d70b15b8db4233b32f55ac02831
olamide folayan <84618342+holladredd@users.noreply.github.com>
2024-07-13 13:45:28 +0100

    Update ClientHome.jsx

--- src/pages/ClientHome.jsx
+++ src/pages/ClientHome.jsx
@@ -59,7 +59,7 @@ const ClientHome = ({ backgrounds }) => {
       <Box
         sx={{
           width: "100%",
-          height: "100vh",
+          height: "90vh",
           overflow: "hidden",
           position: "fixed",
           borderTop: "2px solid #222222a0",
